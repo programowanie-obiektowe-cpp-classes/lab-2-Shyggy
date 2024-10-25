7be2da6c9c21632666031f7567a6eabe705907b1
Jędrzej <mr.myszer@gmail.com>
2024-10-25 22:10:51 +0200

    test

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,25 @@
 #pragma once
-
+#include<iostream>
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* v1;
+
+public:
+    ResourceManager() { v1 = new Resource(); }
+    ~ResourceManager() { delete v1; }
+    ResourceManager(const ResourceManager& a) 
+    { 
+        v1 = new Resource(*a.v1);
+
+    }
+    ResourceManager& operator=(const ResourceManager& a) { v1 = new Resource(*a.v1);
+        return *this;
+    }
+    double get() { return (*v1).get();}
+
 };
+
+
